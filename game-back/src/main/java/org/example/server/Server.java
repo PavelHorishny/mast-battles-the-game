263e1945291df20b2fc06fa8ef372a2751065c2f
Pavel Horishny <horishnypavel@gmail.com>
2025-04-24 14:04:58 +0200

    refactor structure

--- game-back/src/main/java/org/example/server/Server.java
+++ game-back/src/main/java/org/example/server/Server.java
@@ -2,8 +2,8 @@ package org.example.server;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.example.services.GameProcessor;
-import org.example.services.GameService;
+import org.example.services.game.GameProcessor;
+import org.example.services.game.GameService;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
