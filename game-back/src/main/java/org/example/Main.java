c19758f8321620a77de12f81f90113495dcb4aba
Pavel Horishny <horishnypavel@gmail.com>
2025-04-24 13:58:47 +0200

    update

--- game-back/src/main/java/org/example/Main.java
+++ game-back/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.example.server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("web socket server added");
+        System.out.println("game processor added added");
         Server server = new Server(8080);
         server.start();
     }
