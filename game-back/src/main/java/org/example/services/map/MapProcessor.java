162a955af42a61945e8e70f87d2d29aea41f917f
Pavel Horishny <horishnypavel@gmail.com>
2025-04-24 14:21:39 +0200

    Singleton design applied

--- game-back/src/main/java/org/example/services/map/MapProcessor.java
+++ game-back/src/main/java/org/example/services/map/MapProcessor.java
@@ -1,4 +1,22 @@
 package org.example.services.map;
 
 public class MapProcessor {
+
+    private static volatile MapProcessor instance;
+
+    private MapProcessor() {}
+
+    public static MapProcessor getInstance() {
+        MapProcessor result = instance;
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (MapProcessor.class) {
+            if (instance == null) {
+                instance = new MapProcessor();
+            }
+            return instance;
+        }
+    }
 }
