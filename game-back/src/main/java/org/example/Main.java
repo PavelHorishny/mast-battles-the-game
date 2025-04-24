d47e06aa527ec39d3e7a32a6290dc1ac8851638e
Pavel Horishny <horishnypavel@gmail.com>
2025-04-24 13:24:22 +0200

    remove unused import

--- game-back/src/main/java/org/example/Main.java
+++ game-back/src/main/java/org/example/Main.java
@@ -2,10 +2,8 @@ package org.example;
 
 import org.example.server.Server;
 
-import java.net.UnknownHostException;
-
 public class Main {
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         System.out.println("web socket server added");
         Server server = new Server(8080);
         server.start();
