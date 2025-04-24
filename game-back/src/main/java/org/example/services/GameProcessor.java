f4fee2958f6c1a8d8b55a0ff0491433d28b5e02c
Pavel Horishny <horishnypavel@gmail.com>
2025-04-24 13:55:21 +0200

    used singleton design pattern implemented startNewGame method

--- game-back/src/main/java/org/example/services/GameProcessor.java
+++ game-back/src/main/java/org/example/services/GameProcessor.java
@@ -1,4 +1,30 @@
 package org.example.services;
 
-public class GameProcessor {
+public final class GameProcessor implements GameService {
+
+    private static volatile GameProcessor instance;
+
+    private GameProcessor() {
+
+    }
+
+    public static GameProcessor getInstance() {
+        GameProcessor result = instance;
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (GameProcessor.class) {
+            if (instance == null) {
+                instance = new GameProcessor();
+            }
+            return instance;
+        }
+    }
+
+    @Override
+    public String startNewGame() {
+        //System.out.println("Starting a new game...");
+        return "New game started!";
+    }
 }
