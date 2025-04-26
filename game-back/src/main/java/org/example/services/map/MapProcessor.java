97ddec73d69319a86b85c01ee3310b0aea14e9a4
Pavel Horishny <horishnypavel@gmail.com>
2025-04-26 17:17:35 +0200

    initializeMap method added

--- game-back/src/main/java/org/example/services/map/MapProcessor.java
+++ game-back/src/main/java/org/example/services/map/MapProcessor.java
@@ -1,10 +1,18 @@
 package org.example.services.map;
 
+import org.example.map.Coordinates;
+import org.example.map.Surface;
+import org.example.map.SurfaceType;
+import org.example.mockedData.MockedData;
+
 public final class MapProcessor implements MapService {
 
     private static volatile MapProcessor instance;
+    private Surface [][] map = new Surface[34][32];
 
-    private MapProcessor() {}
+    private MapProcessor() {
+        initializeMap(); // temp action
+    }
 
     public static MapProcessor getInstance() {
         MapProcessor result = instance;
@@ -18,5 +26,19 @@ public final class MapProcessor implements MapService {
             }
             return instance;
         }
+    }
+
+    private void initializeMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = new Surface(new Coordinates(i, j), SurfaceType.WATER);
+            }
+        }
+        MockedData.LAND.forEach(coordinates -> map[coordinates.axisX()][coordinates.axisY()] = new Surface(coordinates, SurfaceType.LAND));
+    }
+
+    @Override
+    public Surface[][] getMap() {
+        return map;
     }
 }
