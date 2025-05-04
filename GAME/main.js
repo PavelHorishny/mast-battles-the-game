d834679088ca38b26d33280490b83937be696489
Valerii Roznalievich <vranatolievich@gmail.com>
2025-05-04 18:22:44 +0200

    Add files via upload

---
+++ GAME/main.js
@@ -0,0 +1,12 @@
+function startGame() {
+  console.log("Гра почалась");
+  // redirect or trigger game
+}
+
+function openSettings() {
+  console.log("Налаштування відкрито");
+}
+
+function quitGame() {
+  console.log("Гру закрито");
+}
