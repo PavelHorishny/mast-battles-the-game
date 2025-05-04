b7f5bb376b221b31b2d7e6aa835984b058e9a8a6
Pavel Horishny <horishnypavel@gmail.com>
2025-05-04 16:31:46 +0200

    vessel class added

---
+++ game-back/src/main/java/org/example/game_elements/Vessel.java
@@ -0,0 +1,44 @@
+package org.example.game_elements;
+
+public class Vessel implements Unit,Movable {
+    public Vessel (VesselType vesselType) {
+    }
+
+    public <T> Vessel(T vesselType) {
+    }
+
+    @Override
+    public void place() {
+
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+
+    }
+
+    @Override
+    public void repair() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void makeShot(int damage) {
+
+    }
+
+    @Override
+    public void build() {
+
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+}
