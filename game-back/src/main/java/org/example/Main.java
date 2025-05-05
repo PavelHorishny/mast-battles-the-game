0dbaed21f12cab40ff4323cd8a82506ccb4a5de8
Pavel Horishny <horishnypavel@gmail.com>
2025-05-05 12:42:47 +0200

    update

--- game-back/src/main/java/org/example/Main.java
+++ game-back/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.server.Server;
 public class Main {
     public static void main(String[] args) {
         System.out.println("game processor added added");
+        System.out.println("merging");
         Server server = new Server(8080);
         server.start();
     }
