2564d678ac53a3ba42b942ae32ee7fcac84403e1
Pavel Horishny <horishnypavel@gmail.com>
2025-05-11 22:37:08 +0200

    add method to place units on the map in MapService interface

--- game-back/src/main/java/org/example/services/map/MapService.java
+++ game-back/src/main/java/org/example/services/map/MapService.java
@@ -1,9 +1,13 @@
 package org.example.services.map;
 
+import org.example.game_elements.Unit;
 import org.example.map.Surface;
+
+import java.util.ArrayList;
 
 public interface MapService {
     Surface [] [] getMap();
     void initializeStandardMap();
 
+    void placeUnitsOnMap(ArrayList<Unit> fleet);
 }
