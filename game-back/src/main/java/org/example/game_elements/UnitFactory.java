b5839103056446e7e38c0f8a1f081a85cc23b9ec
Pavel Horishny <horishnypavel@gmail.com>
2025-05-11 18:54:18 +0200

    update UnitFactory: modify create method to include firstPlayer parameter for vessel creation

--- game-back/src/main/java/org/example/game_elements/UnitFactory.java
+++ game-back/src/main/java/org/example/game_elements/UnitFactory.java
@@ -2,15 +2,7 @@ package org.example.game_elements;
 
 public abstract class UnitFactory {
     public static <T> Unit create(boolean firstPlayer, T unitType) {
- /*       if(unitType instanceof VesselType) {
-            return VesselFactory.createVessel((VesselType) unitType);
-        } else if (unitType instanceof FortificationType) {
-            return FortificationFactory.createFortification((FortificationType) unitType);
-        }*/
-/*        return switch (unitType) {
-            case VESSEL -> VesselFactory.createVessel(unitType);
-            case FORTIFICATION -> FortificationFactory.createFortification(unitType);
-        };*/
+
         if(unitType instanceof VesselType){
             return VesselFactory.createVessel(unitType, firstPlayer);
         }else {
