57d7e0c6d464cb25582b604755ee922217629101
Pavel Horishny <horishnypavel@gmail.com>
2025-05-11 17:58:23 +0200

    update UnitProcessor: implement fleet creation logic in setUpAllUnits method

--- game-back/src/main/java/org/example/services/unit/UnitProcessor.java
+++ game-back/src/main/java/org/example/services/unit/UnitProcessor.java
@@ -1,6 +1,9 @@
 package org.example.services.unit;
 
-import org.example.game_elements.FleetSettings;
+import org.example.FleetSettings;
+import org.example.game_elements.*;
+
+import java.util.ArrayList;
 
 public final class UnitProcessor implements UnitService {
     public static volatile UnitProcessor instance;
@@ -22,6 +25,35 @@ public final class UnitProcessor implements UnitService {
 
     @Override
     public void setUpAllUnits(FleetSettings settings) {
+        ArrayList<Unit> firstFleet = new ArrayList<>();
+        firstFleet.addAll(createFleet(settings, true));
+        firstFleet.addAll(createFleet(settings, false));
 
+/*        FortificationType[] tmp = settings.getFortifications();
+        VesselType [] tmp2 = settings.getVessels();
+        for (FortificationType fortificationType : tmp) {
+            firstFleet.add(UnitFactory.create(true, fortificationType));
+            //firstFleet.add(FortificationFactory.createFortification(fortificationType));
+        }
+        for (VesselType vesselType : tmp2) {
+            firstFleet.add(UnitFactory.create(true, vesselType));
+            //firstFleet.add(VesselFactory.createVessel(vesselType));
+        }*/
+
+        // Implementation for setting up all units
+        // This is a placeholder implementation
+        firstFleet.forEach(unit -> System.out.println(unit.toString()));
+        System.out.println(firstFleet.size());
+        System.out.println("Setting up all units with the provided settings.");
+    }
+    private ArrayList<Unit> createFleet(FleetSettings settings, boolean firstPlayer) {
+        ArrayList<Unit> fleet = new ArrayList<>();
+        for (FortificationType fortificationType : settings.getFortifications()) {
+            fleet.add(UnitFactory.create(firstPlayer, fortificationType));
+        }
+        for (VesselType vesselType : settings.getVessels()) {
+            fleet.add(UnitFactory.create(firstPlayer, vesselType));
+        }
+        return fleet;
     }
 }
