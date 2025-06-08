41ce26c3ad9c7e445cb633298b26ea12251a56de
Pavel Horishny <horishnypavel@gmail.com>
2025-06-08 21:29:23 +0200

    remove validation for negative coordinates in Coordinates class

--- game-back/src/main/java/org/example/map/Coordinates.java
+++ game-back/src/main/java/org/example/map/Coordinates.java
@@ -1,9 +1,9 @@
 package org.example.map;
 
 public record Coordinates(int axisX, int axisY) {
-    public Coordinates {
-        if (axisX < 0 || axisY < 0) {
+/*    public Coordinates {
+ *//*       if (axisX < 0 || axisY < 0) {
             throw new IllegalArgumentException("Coordinates cannot be negative");
-        }
-    }
+        }*//*
+    }*/
 }
