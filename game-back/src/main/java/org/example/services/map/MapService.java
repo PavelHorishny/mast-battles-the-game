66b1d91f2feb37ae09cc80b5c8e703eee94fb5ce
Pavel Horishny <horishnypavel@gmail.com>
2025-06-08 21:31:02 +0200

    add methods to place fortifications and vessels on the map

--- game-back/src/main/java/org/example/services/map/MapService.java
+++ game-back/src/main/java/org/example/services/map/MapService.java
@@ -4,10 +4,15 @@ import org.example.game_elements.Unit;
 import org.example.map.Surface;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public interface MapService {
     Surface [] [] getMap();
     void initializeStandardMap();
 
     void placeUnitsOnMap(ArrayList<Unit> fleet);
+
+    void placeFortificationOnMap(Unit fortifications);
+
+    void placeVesselOnMap(Stack<Unit> vessels,ArrayList<Unit> fortifications);
 }
