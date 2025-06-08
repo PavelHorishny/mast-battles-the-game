7d84dd33a94f5bab815d42818a42047db4a3b8c3
Pavel Horishny <horishnypavel@gmail.com>
2025-06-08 21:31:30 +0200

    refactor UnitProcessor to place fortifications and vessels on the map

--- game-back/src/main/java/org/example/services/unit/UnitProcessor.java
+++ game-back/src/main/java/org/example/services/unit/UnitProcessor.java
@@ -5,10 +5,14 @@ import org.example.game_elements.*;
 import org.example.services.map.MapProcessor;
 import org.example.services.map.MapService;
 import org.example.services.name.NameProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public final class UnitProcessor implements UnitService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UnitProcessor.class);
     public static volatile UnitProcessor instance;
     private final MapService mapProcessor;
 
@@ -35,19 +39,57 @@ public final class UnitProcessor implements UnitService {
         fleet.addAll(createFleet(settings, false));
 
         fleet.forEach(unit -> NameProcessor.getInstance().getName(unit));
-        mapProcessor.placeUnitsOnMap(fleet);
-        fleet.forEach(unit -> System.out.println(unit.toString()));
-        System.out.println(fleet.size());
+        //mapProcessor.placeUnitsOnMap(fleet);
+        //fleet.forEach(unit -> System.out.println(unit.toString()));
+        //System.out.println(fleet.size());
         System.out.println("Setting up all units with the provided settings.");
     }
     private ArrayList<Unit> createFleet(FleetSettings settings, boolean firstPlayer) {
 
-        ArrayList<Unit> fleet = new ArrayList<>();
+        ArrayList<Unit> fortifications = new ArrayList<>();
         for (FortificationType fortificationType : settings.getFortifications()) {
-            fleet.add(UnitFactory.create(firstPlayer, fortificationType));
+            fortifications.add(UnitFactory.create(firstPlayer, fortificationType));
         }
-        for (VesselType vesselType : settings.getVessels()) {
-            fleet.add(UnitFactory.create(firstPlayer, vesselType));
+        fortifications.forEach(mapProcessor::placeFortificationOnMap);
+
+        /**
+         * Algorithm to create vessels
+         * get standard fleet from settings
+         * shuffle it
+         * create vessels
+         * put a shuffled list to stack
+         * place vessels on a map
+         * add a vessel to port*/
+        //Collections.shuffle(Arrays.asList(settings.getVessels()));
+        ArrayList<VesselType> vesselTypes = new ArrayList<>(Arrays.asList(settings.getVessels()));
+        Collections.shuffle(vesselTypes);
+        Stack<Unit> vesselsStack = new Stack<>();
+        ArrayList<Unit> vessels = new ArrayList<>();
+
+        for(VesselType vesselType : vesselTypes) {
+            Unit vessel = UnitFactory.create(firstPlayer, vesselType);
+            vessels.add(vessel);
+            vesselsStack.push(vessel);
+        }
+        mapProcessor.placeVesselOnMap(vesselsStack, fortifications);
+        //vessels.forEach(unit -> logger.debug(unit.toString()));
+        //TODO : add logging
+
+
+      /*  for (VesselType vesselType : settings.getVessels()) {
+            vessels.add(UnitFactory.create(firstPlayer, vesselType));
+        }
+        mapProcessor.placeVesselOnMap(shufflePlayersVessels(vessels),fortifications);
+        fleet.addAll(shufflePlayersVessels(vessels));*/
+        return new ArrayList<>(fortifications);
+    }
+    private Stack <Unit> shufflePlayersVessels(ArrayList<Unit> vessels){
+        Stack <Unit> fleet = new Stack<>();
+        int end = vessels.size();
+        while (fleet.size()<end){
+            int x = (int) (Math.random()*(vessels.size()));
+            fleet.push(vessels.get(x));
+            vessels.remove(x);
         }
         return fleet;
     }
